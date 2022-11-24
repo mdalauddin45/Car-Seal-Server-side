457ce1e802ef1ba4fa685be3fa7b3a9bfde70f01
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-11-24 10:39:06 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-const corsConfig = {
-  origin: "",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-
-app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
+app.use(cors());
 app.use(express.json());
+
+//MongoDb Add
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.teba24n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Assignment server is running");
