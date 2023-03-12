e8ab433cd488aafb11a0cd85a01d362785f7dce8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-03-12 22:23:38 +0600

    practice

---
+++ controllers/userocntroller/users.Controller.js
@@ -0,0 +1,11 @@
+// const getAllUsers = (res, req) => {
+//     res.send('all user pai gai');
+// }
+
+// module.exports = {
+//     getAllUsers
+// }
+
+module.exports.getAllUsers = (req, res) => {
+    res.send('all user pai gai');
+}
